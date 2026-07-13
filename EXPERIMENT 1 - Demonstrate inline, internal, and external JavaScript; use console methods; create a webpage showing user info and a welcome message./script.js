31603eedaa68768cfdda2a3642f9016a9eb50085
Anaya Shende <anayashende2006@gmail.com>
2026-07-14 01:14:54 +0530

    Add files via upload

---
+++ EXPERIMENT 1 - Demonstrate inline, internal, and external JavaScript; use console methods; create a webpage showing user info and a welcome message./script.js
@@ -0,0 +1,12 @@
+function displayUserInfo() {
+    alert(
+        "Name: Anaya Shende\n" +
+        "Course: B.Tech Computer Science\n" +
+        "PRN: 24070521213"
+    );
+
+    console.log("Name: Anaya Shende");
+    console.info("Course: B.Tech Computer Science");
+    console.warn("PRN information displayed.");
+    console.error("Console error demonstration.");
+}
